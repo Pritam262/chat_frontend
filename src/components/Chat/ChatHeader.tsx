8e3e7bc5977db1475417af03fbe3ef621319372a
Pritam <pritamjana360@gmail.com>
2024-05-12 21:56:15 +0530

    V-0.2.3
    Update chat header component with chat user name and profile picture

---
+++ src/components/Chat/ChatHeader.tsx
@@ -0,0 +1,28 @@
+import { MdCall } from "react-icons/md";
+import Avatar from "../Common/Avatar";
+import { IoVideocam } from "react-icons/io5";
+import { BiSearchAlt } from "react-icons/bi";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { useAppContext } from "@/context/appContext";
+
+export default function ChatHeader() {
+    const { currentChatUser } = useAppContext();
+    return <div className="h-16 px-4 py-3 flex justify-between items-center bg-panel-header-background z-10">
+        {currentChatUser && <>
+            <div className="flex items-center-justify-center gap-6">
+                <Avatar props={{ type: "sm", image: currentChatUser?.profilePicture, setImage: () => "" }} />
+                <div className="flex flex-col">
+                    <span className="text-primary-strong">{currentChatUser?.name}</span>
+                    <span className="text-secondary text-sm">Online</span>
+                </div>
+            </div>
+            <div className="flex gap-6">
+                <MdCall className="text-panel-header-icon cursor-pointer text-xl" />
+                <IoVideocam className="text-panel-header-icon cursor-pointer text-xl" />
+                <BiSearchAlt className="text-panel-header-icon cursor-pointer text-xl" />
+                <BsThreeDotsVertical className="text-panel-header-icon cursor-pointer text-xl" />
+            </div>
+        </>
+        }
+    </div>
+}
