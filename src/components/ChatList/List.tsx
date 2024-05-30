b9d388734f73bbeb222b2326270f08002a2970b8
Pritam <pritamjana360@gmail.com>
2024-05-30 23:44:43 +0530

    V-0.6.9
    Update chatList component UI

--- src/components/ChatList/List.tsx
+++ src/components/ChatList/List.tsx
@@ -1,5 +1,25 @@
+import { useAppContext } from "@/context/appContext"
+import { GET_INITIAL_CONTACTS_ROUTE } from "@/utils/ApiRoutes";
+import axios from "axios";
+import { useEffect } from "react";
+import ChatListItem from "./ChatListItem";
 export default function List() {
+    const { userInfo, setUserContacts, setOnlineUsers, userContacts, isSearchChatActive, filterContacts } = useAppContext();
+    useEffect(() => {
+        const getContacts = async () => {
+            try {
+                const { data: { users, onlineUsers } } = await axios.get(`${GET_INITIAL_CONTACTS_ROUTE}/${userInfo?.id}`)
+                // console.log("Users", users);
+                // console.log("OnlineUsers", onlineUsers);
+                setUserContacts(users);
+                setOnlineUsers(onlineUsers);
+            } catch (error) {
+                console.error(error)
+            }
+        };
+        if (userInfo?.id) getContacts();
+    }, [userInfo])
     return <div className="bg-search-input-container-background flex-auto overflow-auto max-h-full custom-scrollbar">
-        
+        {isSearchChatActive ? filterContacts?.map((contact, index) => <ChatListItem props={{ data: contact, isContactPage: false, totalUnreadMessages: contact?.totalUnreadMessages, contact: contact }} key={index} />) : userContacts?.map((contact, index) => <ChatListItem props={{ data: contact, isContactPage: false, totalUnreadMessages: contact?.totalUnreadMessages, contact: contact }} key={index} />)}
     </div>
 }
