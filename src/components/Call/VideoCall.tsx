6246573c7271818e94341e832be1aa4d7fe279ee
Pritam <pritamjana360@gmail.com>
2024-06-04 23:57:25 +0530

    V-0.8.7
    Add video call Component

---
+++ src/components/Call/VideoCall.tsx
@@ -0,0 +1,24 @@
+import { IoMdCloseCircle } from "react-icons/io";
+import { useAppContext } from "@/context/appContext";
+import Container from "./Container";
+import { useEffect } from "react";
+
+export default function VideoCall() {
+    const {  videoCall, socket, userInfo, } = useAppContext();
+    useEffect(() => {
+        if (videoCall?.type === "out-going") {
+            socket?.emit("outgoing-video-call", {
+                to: videoCall?.currentChatUser?.id,
+                from: {
+                    id: userInfo?.id,
+                    profilePicture: userInfo?.photoURL,
+                    name: userInfo?.displayName,
+                },
+                callType: videoCall?.callType,
+                roomId: videoCall?.roomId,
+            })
+        }
+    }, [videoCall])
+
+    return <Container props={{ data: videoCall }} />
+}
