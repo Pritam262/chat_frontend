3fbd5313888e647d719204ead9c8aa2e484777a3
Pritam <pritamjana360@gmail.com>
2024-06-04 23:56:13 +0530

    V-0.8.5
    Update ChatHeader component where user can call videocall and video call

--- src/components/Chat/ChatHeader.tsx
+++ src/components/Chat/ChatHeader.tsx
@@ -4,9 +4,41 @@ import { IoVideocam } from "react-icons/io5";
 import { BiSearchAlt } from "react-icons/bi";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { useAppContext } from "@/context/appContext";
+import { useState, MouseEvent } from "react";
+import ContextMenu from "../Common/ContextMenu";
 
 export default function ChatHeader() {
-    const { currentChatUser, setMessagesSearch, onlineUsers } = useAppContext();
+    const { currentChatUser, setMessagesSearch, onlineUsers, setVideoCall, setVoiceCall,  exitChat } = useAppContext();
+
+    const [isContextMenuVisible, setIsContextMenuVisible] = useState(false);
+    const [contextMenuCordinate, setContextMenuCordinates] = useState({ x: 0, y: 0 });
+
+
+    const showContextMenu = (e: MouseEvent<HTMLElement>) => {
+        setContextMenuCordinates({ x: e.pageX - 50, y: e.pageY + 20 })
+        setIsContextMenuVisible(true);
+    }
+
+    const contextMenuOption = [
+        {
+            name: "Exit", callback: () => {
+                exitChat();
+            }
+        },
+    ]
+
+    const handleVoiceCall = () => {
+        currentChatUser && setVoiceCall({ currentChatUser, type: "out-going", callType: "voice", roomId: Date.now() });
+
+        setVideoCall(undefined);
+    };
+
+    const handleVideoCall = () => {
+        currentChatUser && setVideoCall({ currentChatUser, type: "out-going", callType: "video", roomId: Date.now() });
+
+        setVoiceCall(undefined);
+    };
+
     return <div className="h-16 px-4 py-3 flex justify-between items-center bg-panel-header-background z-100">
         {currentChatUser && <>
             <div className="flex items-center-justify-center gap-6">
@@ -17,10 +49,26 @@ export default function ChatHeader() {
                 </div>
             </div>
             <div className="flex gap-6">
-                <MdCall className="text-panel-header-icon cursor-pointer text-xl" />
-                <IoVideocam className="text-panel-header-icon cursor-pointer text-xl" />
+                <MdCall className="text-panel-header-icon cursor-pointer text-xl" onClick={handleVoiceCall} />
+                <IoVideocam className="text-panel-header-icon cursor-pointer text-xl" onClick={handleVideoCall} />
                 <BiSearchAlt className="text-panel-header-icon cursor-pointer text-xl" onClick={() => setMessagesSearch(true)} />
-                <BsThreeDotsVertical className="text-panel-header-icon cursor-pointer text-xl" />
+                <div onClick={showContextMenu} id="context-opener" >
+
+                    <BsThreeDotsVertical className="text-panel-header-icon cursor-pointer text-xl" />
+                </div>
+
+                {isContextMenuVisible && (
+                    <ContextMenu props={
+                        {
+                            options: contextMenuOption,
+                            cordinates: contextMenuCordinate,
+                            contextMenu: isContextMenuVisible,
+                            setContextMenu(value) {
+
+                            },
+                        }
+                    } />
+                )}
             </div>
         </>
         }
