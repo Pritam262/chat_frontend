6ca22036406188d3b6a446c18be6afeae1477a4a
Pritam <pritamjana360@gmail.com>
2024-06-04 23:45:04 +0530

    V-0.7.8
    Add GET initial contact or message and generate token routes

--- src/utils/ApiRoutes.ts
+++ src/utils/ApiRoutes.ts
@@ -1,13 +1,17 @@
 export const HOST = "http://localhost:3000";
 const AUTH_ROUTE = `${HOST}/api/auth`;
 const MESSAGES_ROUTE = `${HOST}/api/messages`
-export const CHECK_USER_ROUTE = `${AUTH_ROUTE}/check-user`;
 
+export const CHECK_USER_ROUTE = `${AUTH_ROUTE}/check-user`;
 export const ONBOARD_USER_ROUTE = `${AUTH_ROUTE}/onboard-user`;
 export const FETCH_ALL_USER = `${AUTH_ROUTE}/get-contacts`;
+export const GET_GENERATE_TOKEN =  `${AUTH_ROUTE}/generate-token/`;
+
+
+
 export const ADD_MESSAGES_ROUTE = `${MESSAGES_ROUTE}/add-message`
 export const GET_MESSAGES_ROUTE = `${MESSAGES_ROUTE}/get-messages`
 export const SEND_IMAGE_ROUTE = `${MESSAGES_ROUTE}/add-image-msg`;
 export const SEND_AUDIO_MESSAGE_ROUTE = `${MESSAGES_ROUTE}/add-audio-msg`;
+export const GET_INITIAL_CONTACTS_ROUTE = `${MESSAGES_ROUTE}/get-initial-contacts`;
 
-export const GET_INITIAL_CONTACTS_ROUTE = `${MESSAGES_ROUTE}/get-initial-contacts`
